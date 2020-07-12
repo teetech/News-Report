435b78613f5b3a83ea3b74abefb8c164aa0d08f7
Anthony Awuzie <anthonychigo@yahoo.com>
2020-07-11 20:32:50 -0500

    Initial Commit

---
+++ News Report/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  News Report
+//
+//  Created by Anthony Awuzie on 7/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ News Report/News_ReportApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_ReportApp.swift
+//  News Report
+//
+//  Created by Anthony Awuzie on 7/11/20.
+//
+
+import SwiftUI
+
+@main
+struct News_ReportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
